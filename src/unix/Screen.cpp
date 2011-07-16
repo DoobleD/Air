d00eece3c8e7354e2b929d791eb6dcc051c08ada
DoobleD <pierre.alletru@gmail.com>
2011-07-16 14:49:52 +0100

    git forget a file?

--- src/unix/Screen.cpp
+++ src/unix/Screen.cpp
@@ -1,33 +1,31 @@
-#include <X11/Xlib.h>
-
 #include "Screen.hpp"
 
 // No use of 'using namespace air::os' to avoid conflict with a struct Screen form Xlib
 
+air::os::Screen::Screen(void)
+{
+  m_display = XOpenDisplay(0);
+  m_rootWindow = XRootWindow(m_display, 0);
+}
+
+air::os::Screen::~Screen(void)
+{
+}
+
 int			air::os::Screen::getResX(void)
 {
-  Display *		display;
-  Window		root_window;
   XWindowAttributes 	attributes;
 
-  display = XOpenDisplay(0);
-  root_window = XRootWindow(display, 0);
-  
-  XGetWindowAttributes(display, root_window, &attributes);
+  XGetWindowAttributes(m_display, m_rootWindow, &attributes);
 
   return attributes.width;
 }
 
 int			air::os::Screen::getResY(void)
 {
-  Display *		display;
-  Window		root_window;
   XWindowAttributes 	attributes;
 
-  display = XOpenDisplay(0);
-  root_window = XRootWindow(display, 0);
-  
-  XGetWindowAttributes(display, root_window, &attributes);
+  XGetWindowAttributes(m_display, m_rootWindow, &attributes);
 
-  return attributes.width;
+  return attributes.height;
 }
