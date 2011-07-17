d1a3ab6985b5f60ebadf0a2f58e7a439cd456494
DoobleD <pierre.alletru@gmail.com>
2011-07-17 20:27:24 +0100

    CMakeLists now compiles only .cpp & .hpp

--- include/E_Maximize.hpp
+++ include/E_Maximize.hpp
@@ -1,4 +1,25 @@
-<hr class="footer"/><address class="footer"><small>
-Generated on $datetime for $projectname by&#160;<a href="http://www.doxygen.org/index.html"><img class="footer" src="$relpath$doxygen.png" alt="doxygen"/></a> $doxygenversion</small></address>
-</body>
-</html>
+#ifndef _E_MAXIMIZE_HPP_
+#define _E_MAXIMIZE_HPP_
+
+// NITE headers
+#include <XnVSwipeDetector.h>
+
+#define MIN_SPEED			0.75 // m/s
+
+namespace air
+{
+
+  class					E_Maximize : public XnVSwipeDetector
+  {
+  public:
+    E_Maximize();
+    ~E_Maximize();
+    
+    static void XN_CALLBACK_TYPE	onSwipe(XnFloat velocity, 
+						XnFloat angle, 
+						void *userCxt);
+  };
+
+}
+
+#endif //_E_MAXIMIZE_HPP_
