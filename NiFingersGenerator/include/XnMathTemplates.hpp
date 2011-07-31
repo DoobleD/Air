e9432cd1594a8dbcd91c1076db22c6c0ebe95a28
DoobleD <pierre.alletru@gmail.com>
2011-07-31 21:23:11 +0100

    Forgot to add a new file.

---
+++ NiFingersGenerator/include/XnMathTemplates.hpp
@@ -0,0 +1,35 @@
+#ifndef _XN_MATH_TEMPLATES_HPP_
+#define _XN_MATH_TEMPLATES_HPP_
+
+namespace xn
+{
+  namespace math
+  {
+    template<typename T> 
+    float		PointsClosest2D_BruteForce(typename list<T>::iterator begin,
+						   typename list<T>::iterator end, 
+						   const T & point,
+						   typename list<T>::iterator & closest)
+    {
+      float		dist;
+      float		minDist;
+  
+      minDist = -1;
+      while (begin != end)
+	{
+	  // Force 2D distance calculation, no matter what is the nb of dimension of T
+	  dist = PointsDistance((Point2Df) *begin, (Point2Df) point);
+	  if (minDist == -1 || dist < minDist)
+	    {
+	      minDist = dist;
+	      closest = begin;
+	    }
+      
+	  ++begin;
+	}
+  
+      return minDist;
+    }
+  }
+}
+#endif //_XN_MATH_TEMPLATES_HPP_
